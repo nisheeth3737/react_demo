1d6ba0fb815440f25fadea0ffe1cfad1662fb18b
nisheeth3737 <141109275+nisheeth3737@users.noreply.github.com>
2023-08-12 15:39:06 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
 import './style.css';
-
 import Create from './components/create';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">React Crud Operations</h2>
-      <div>
-        <Create />
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Create />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
-export default App;
 
+export default App;
